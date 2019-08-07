2e22f4709aa416097c383f8d98399efdd5125a41
nickborbe <nickborbe@gmail.com>
2019-08-07 15:24:01 -0400

    added built files to public folder

---
+++ server/public/precache-manifest.f0cf2f03af87031deef47299d1799ffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac3537e3f16a2cae3b904a7f2877fc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88e0cc1217fcb5c554a0",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2898a344ed7b42519541",
+    "url": "/static/js/2.f668b586.chunk.js"
+  },
+  {
+    "revision": "88e0cc1217fcb5c554a0",
+    "url": "/static/js/main.b62b1381.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
